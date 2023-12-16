df7033ca4a9ce98fa2b88cfa60817349059fe935
AbdelrahmanWael2 <abdelrahmanwael50@gmail.com>
2023-12-16 16:11:34 +0200

    first commit

---
+++ client/client.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <cstdint>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cinttypes>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <vector>
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+#define MAX_BUFFER_SIZE 1024
+
+typedef struct packet{
+    uint16_t cksum;
+    uint16_t len;
+    uint32_t seq;
+    //Data
+    char data[MAX_BUFFER_SIZE] ;
+} p;
+
+typedef struct ack_packet{
+    uint16_t cksum;
+    uint16_t len;
+    uint32_t ackno;
+} a;
+
+int sock_fd = 0;
+struct sockaddr_in serv_addr;
+//ip, port, filename
+vector<string> clientData(3);
+p *pck;
+
+socklen_t fromlen;
+long long byte_count = 0;
+
+void readClientData();
+
+
+uint16_t calculateChecksum(const char* data, uint16_t len) {
+    uint32_t sum = 0;
+    for (uint16_t i = 0; i < len; ++i) {
+        sum += data[i];
+    }
+    while (sum >> 16) {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+    return static_cast<uint16_t>(~sum);
+}
+
+
+void initializeClient(){
+    printf("* Client starting ... \n");
+    pck = (packet *)malloc(sizeof(packet));
+    
+    // Creating socket file descriptor
+    if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) { //type of socket is Datagram
+        printf("\n Socket creation error \n");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    readClientData();
+
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(stoi(clientData[1])); //8080
+
+    // setting the address given (127.0.0.1)
+    if (inet_pton(AF_INET, clientData[0].c_str(), &serv_addr.sin_addr) == -1) {
+        printf("\n Invalid address\n");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(pck->data, clientData[2].c_str());
+    pck->len = clientData[2].size();
+    
+
+    //check sum
+    pck->cksum = calculateChecksum(pck->data, pck->len);
+
+    if ((sendto(sock_fd, pck->data, pck->len, 0,
+                (struct sockaddr *)&serv_addr, sizeof(serv_addr))) == -1) {
+        printf("\n Failed to send packet data from Client \n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void readClientData() {
+    ifstream infile("client.in");
+    string line;
+    int i = 0;
+    while (getline(infile, line))
+        clientData[i++] = line;
+}
+
+void receiveServerData(){
+
+    // open output file for server data
+    ofstream wf("server.out", ios::out | ios::binary);
+    if (!wf) {
+        cout << "Cannot open file!" << endl;
+        return;
+    }
+    uint16_t last_received= 0; //sequence number of the last recieved packet
+    while (true) {
+        fromlen = sizeof serv_addr;
+        packet packet;
+        ack_packet ack;
+        // receive data in buffer
+        if ((byte_count = recvfrom(sock_fd, &packet, sizeof(packet) , 0,
+                                (struct sockaddr *)&serv_addr, &fromlen)) < 1)
+        {
+            printf("File received successfully \n");
+            break;
+        }
+        printf("Received packet with SEQ %d and LEN %d\n", packet.seq, packet.len);
+        if (packet.seq <= last_received) {
+            // duplicate packet
+            ack.ackno = last_received + 1;
+        } else {
+            // write received data
+            wf.write(packet.data, packet.len);
+            wf.flush();
+            ack.ackno = packet.seq + 1;
+            last_received= packet.seq;
+        }
+        // Send acknowledgement after receiving and consuming a data packet
+        ack.len = 0;
+
+        
+        sendto(sock_fd, &ack, sizeof(ack), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    }
+    wf.close();
+    if (!wf.good()) {
+        cout << "Error occurred at writing time!" << endl;
+        return;
+    }
+}
+
+int main(){
+    initializeClient();
+    receiveServerData();
+    free(pck);
+    close(sock_fd);
+    return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,310 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <thread>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <vector>
+#include <numeric>
+#include <fstream>
+#include <algorithm>
+#define MSS 16 // Maximum Segment Size
+
+using namespace std;
+
+const int TIMEOUT_SECONDS = 5;
+// const int PORT = 8080;
+const int BUFFER_SIZE = 16;
+const int INITIAL_CWND = 1;
+int PORT;
+int SEED;
+float PROBABILITY_LOSS;
+
+typedef struct packet
+{
+    /* Header */
+    uint16_t chsum;
+    uint16_t len;
+    uint16_t seqno;
+    /* Data */
+    char data[MSS];
+} packet;
+
+typedef struct ack_packet
+{
+    uint16_t chsum;
+    uint16_t ackno;
+    uint16_t len;
+} ack_packet;
+
+typedef struct MessageArgs
+{
+    sockaddr_in client_address{};
+    string filePath;
+} MessageArgs;
+
+uint16_t calculateChecksum(const char *data, uint16_t len)
+{
+    uint32_t sum = std::accumulate(data, data + len, 0);
+    while (sum >> 16)
+    {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+    return static_cast<uint16_t>(~sum);
+}
+
+packet make_packet(uint16_t seqno, uint16_t len, char data[])
+{
+    packet p;
+    p.chsum = 0;
+    p.len = len;
+    p.seqno = seqno + 2;
+
+    p.chsum = calculateChecksum(data, len);
+
+    strcpy(p.data, data);
+    return p;
+}
+
+// read the contents of a file into a vector of packet structs
+vector<packet> readFile(char *fileName) {
+    FILE *fp;
+    vector<packet> packets;
+    char *content = (char *)malloc(10000);
+    fp = fopen(fileName, "rb");
+    if (fp == nullptr)
+        return packets;
+    int nBytes = 0;
+    int seqno = 0;  // Initialize sequence number
+    while (fread(&content[nBytes], sizeof(char), 1, fp) == 1) {
+        nBytes++;
+        if (nBytes == MSS) {
+            packet p = make_packet(seqno++, nBytes, content);
+            packets.push_back(p);
+            nBytes = 0;
+        }
+    }
+    if (nBytes != 0) {
+        packet p = make_packet(seqno, nBytes, content);
+        packets.push_back(p);
+    }
+    fclose(fp);
+    free(content);
+    return packets;
+}
+
+
+// wait for a specified amount of time for a socket to become readable
+int timeOut(int sockfd)
+{
+    fd_set read_fds;
+    FD_ZERO(&read_fds);
+    FD_SET(sockfd, &read_fds);
+    // Set up the timeout
+    struct timeval timeout
+    {
+    };
+    timeout.tv_sec = TIMEOUT_SECONDS;
+    timeout.tv_usec = 0;
+
+    // Wait for the socket to become readable or for the timeout to expire
+    int status = select(sockfd + 1, &read_fds, nullptr, nullptr, &timeout);
+
+    return status;
+}
+
+void sendDataChunks(int sockfd, sockaddr_in client_address, char *fileName)
+{
+    vector<packet> packets = readFile(fileName);
+    unsigned int n = packets.size();
+
+    // Congestion Control Variables
+    int cwnd = INITIAL_CWND;
+    int ssthresh = 64; // ssthresh
+    int dupACKcount = 0;
+
+    for (int i = 0; i < static_cast<int>(n);)
+    {
+        if ((SEED % 100) < (PROBABILITY_LOSS * 100))
+        {
+            cout << "Simulating packet loss for packet with seqno: " << packets[i].seqno << endl;
+            i++;
+            continue; // Skip sending this packet
+        }
+
+        // Sending packets within the congestion window
+        for (int j = i; j < min(i + cwnd, static_cast<int>(n)); ++j)
+        {
+            sendto(sockfd, &packets[j], sizeof(packet), 0,
+                   (sockaddr *)&client_address, sizeof(client_address));
+        }
+
+        // wait acknowledgement from client
+        ack_packet ack;
+        socklen_t client_addr_len = sizeof(client_address);
+        int status = timeOut(sockfd);
+        if (status == -1)
+        {
+            // An error occurred
+            cerr << "Error waiting for socket: " << strerror(errno) << endl;
+            return;
+        }
+        else if (status == 0)
+        {
+            // The timeout expired
+            cerr << "Timeout expired" << endl;
+
+            // Implement timeout actions: Set ssthresh, reduce cwnd, and go to Slow Start
+            ssthresh = cwnd / 2;
+            cwnd = INITIAL_CWND;
+            i -= cwnd; // Go back and retransmit
+        }
+        else
+        {
+            // ACK received
+            long bytes_received = recvfrom(sockfd, &ack, sizeof(ack), 0,
+                                           (sockaddr *)&client_address, &client_addr_len);
+
+            if (bytes_received <= 0)
+            {
+                break;
+            }
+
+            // Check for duplicate ACKs
+            if (ack.ackno == packets[i].seqno)
+            {
+                dupACKcount++;
+            }
+            else
+            {
+                dupACKcount = 0;
+            }
+
+            // Update cwnd based on Slow Start, Congestion Avoidance, and Fast Recovery
+            if (dupACKcount == 3)
+            {
+                // Fast Recovery
+                ssthresh = cwnd / 2;
+                cwnd = ssthresh + 3 * MSS;
+            }
+            else if (dupACKcount > 3)
+            {
+                // Avoid excessive increase during Fast Recovery
+                cwnd += MSS;
+            }
+            else
+            {
+                // Slow Start or Congestion Avoidance
+                cwnd += (dupACKcount < 3) ? cwnd : MSS;
+            }
+
+            // Move to the next unacknowledged packet
+            i = min(i + cwnd, static_cast<int>(n));
+        }
+    }
+}
+
+
+void handle_connection(void *args)
+{
+    // Get the socket and client address from the arguments
+    int newSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (newSocket < 0)
+    {
+        cerr << "Error creating socket" << endl;
+        return;
+    }
+
+    auto *message_args = (MessageArgs *)args;
+    sockaddr_in client_address = message_args->client_address;
+    string filePath = message_args->filePath;
+    // should handle the send of data in chunks
+    sendDataChunks(newSocket, client_address, (char *)filePath.c_str());
+    // Close the connection
+    close(newSocket);
+}
+
+void waitForConnection(int sockfd)
+{
+    while (true)
+    {
+        // Wait for a connection
+        char buffer[BUFFER_SIZE];
+        cout << "Server waiting for a connection..." << endl;
+        sockaddr_in client_addr{};
+        socklen_t client_addr_len = sizeof(client_addr);
+        long bytes_received = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
+                                       (sockaddr *)&client_addr, &client_addr_len);
+        if (bytes_received < 0)
+        {
+            cerr << "Failed to accept connection" << endl;
+            continue;
+        }
+
+        // Create a new thread to handle the connection
+        MessageArgs messageArgs;
+        messageArgs.client_address = client_addr;
+        string str;
+        str = buffer;
+        messageArgs.filePath = str.substr(0, bytes_received);
+        pthread_t thread;
+        pthread_create(&thread, nullptr, reinterpret_cast<void *(*)(void *)>(handle_connection), &messageArgs);
+    }
+}
+
+bool readServerConfig(const string &filename)
+{
+    ifstream inputFile(filename);
+    if (!inputFile.is_open())
+    {
+        cerr << "Error opening file: " << filename << endl;
+        return false;
+    }
+
+    // Read parameters from the file
+    if (!(inputFile >> PORT >> SEED >> PROBABILITY_LOSS))
+    {
+        cerr << "Error reading parameters from file" << endl;
+        return false;
+    }
+
+    // Seed the random number generator
+    srand(SEED);
+
+    inputFile.close();
+    return true;
+}
+
+int main()
+{
+    if (!readServerConfig("server.in"))
+    {
+        return 1;
+    }
+
+    // Create a socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+    {
+        cerr << "Error creating socket" << endl;
+        return 1;
+    }
+
+    // bind the socket to the port
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+    if (bind(sockfd, (sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        cerr << "Error binding socket to port" << endl;
+        return 1;
+    }
+
+    // waiting for connections
+    waitForConnection(sockfd);
+
+    return 0;
+}
